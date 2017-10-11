9561216e1bbcd066e436498d067a9855dc0b2086
syyang <beyanger@yeah.net>
2017-10-11 16:20:08 +0800

    2.cpp

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ LeetCode
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
-|1|[Two Sum](https://leetcode.com/problems/two-sum/description/)| [C++](./algorithm/cpp/1.cpp)|Easy|
+|2|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers/description/)| [C++](./algorithm/cpp/2.cpp)|Medium|
+|1|[Two Sum](https://leetcode.com/problems/two-sum/description/)| [C++](./algorithm/cpp/1.cpp)|Medium|
+
 
---
+++ algorithm/cpp/2.cpp
@@ -0,0 +1,24 @@
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+	ListNode *helper(ListNode *l1, ListNode *l2, int carry) {
+		if (!l1 && !l2 && !carry) return NULL;
+		if (l1) { carry += l1->val; l1 = l1->next;}
+		if (l2) { carry += l2->val; l2 = l2->next;}
+		ListNode *node = new ListNode(carry % 10);
+		node->next = helper(l1, l2, carry/10);
+		return node;
+	}
+public:
+	ListNode* addTwoNumbers(ListNode *l1, ListNode* l2) {
+		return helper(l1, l2, 0);
+	}
+};
