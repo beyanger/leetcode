3bdc37e724e2c0053c7ba4b67334bbc50c44e79f
syyang <beyanger@yeah.net>
2017-10-11 18:39:36 +0800

    690.cpp

---
+++ algorithm/cpp/690.cpp
@@ -0,0 +1,24 @@
+
+/*
+// Employee info
+class Employee {
+public:
+	// It's the unique ID of each node.
+	// unique id of this employee
+	int id;
+	// the importance value of this employee
+	int importance;
+	// the id of direct subordinates
+	vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+	int getImportance(vector<Employee*>& employees, int id) {
+		int res = employees[id-1]->importance;
+		for (int em : employees[id-1]->subordinates) 
+			res += getImportance(employees, em);
+		return res;
+	}
+};
