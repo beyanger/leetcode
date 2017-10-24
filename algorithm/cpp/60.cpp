ec53be1bf2bc802d3dbc0ba0abaabb860b9a4dff
syyang <beyanger@yeah.net>
2017-10-24 10:58:56 +0800

    fix bug

--- algorithm/cpp/60.cpp
+++ algorithm/cpp/60.cpp
@@ -3,22 +3,19 @@ class Solution {
 public:
 	string getPermutation(int n, int k) {
 		const static int fact[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
-		const static char *str = "123456789";
+		const static char *s = "123456789";
 
-		string s(n, '0');
-		for (int i = 0; i < n; i++)
-			s[i] = str[i];
-
+		string str(s, s+n);
 		k--;
 		string res = "";
 		for (int i = n-1; i >= 0; i--) {
 			int m = (k / fact[i]);
 			k  %= fact[i];
-			res.push_back(s[m]);
-			s.erase(s.begin()+m);
+			res.push_back(str[m]);
+			str.erase(str.begin()+m);
 		}
 
-		return res; 
+		return res;
 	}
 };
 
