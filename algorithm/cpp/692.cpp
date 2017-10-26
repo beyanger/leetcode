bfc749bae7386792170630fb08d249cc1d3aae8c
syyang <beyanger@yeah.net>
2017-10-26 10:31:14 +0800

    modify 692.cpp

--- algorithm/cpp/692.cpp
+++ algorithm/cpp/692.cpp
@@ -1,25 +1,25 @@
 
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
 class Solution {
-    struct KV {
-        string s;
-        int f;
-        KV(string s, int f) : s(s), f(f) {}
-    };
 
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string, int> w;
         for (string& s :words) w[s]++;
-        vector<KV *> v;
+        vector<pair<string, int> *> v;
         for (auto itr = w.begin(); itr!=w.end(); itr++)
-            v.push_back(new KV(itr->first, itr->second));
-        sort(v.begin(), v.end(), [](const KV* a, const KV* b) {
-                if (a->f != b->f) return a->f > b->f;
-                return a->s < b->s;
+            v.push_back(new pair<string, int>(itr->first, itr->second));
+        sort(v.begin(), v.end(), [](const pair<string, int>* a, const pair<string, int>* b) {
+                if (a->second != b->second) return a->second > b->second;
+                return a->first < b->first;
                 });
         vector<string> res;
         for (int i = 0; i < k; i++) 
-            res.push_back(v[i]->s);
+            res.push_back(v[i]->first);
         for (auto kv : v) delete kv;
         return res;
     }
